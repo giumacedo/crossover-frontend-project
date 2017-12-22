fdb0160837c8ec8c25de087ce0cecbc3cff10fde
Giulliano Macedo <giulli.macedo@gmail.com>
2017-12-22 18:25:22 -0200

    update table component

--- src/js/Table.jsx
+++ src/js/Table.jsx
@@ -3,10 +3,42 @@ import preload from '../../data.json';
 
 class Table extends Component {
   state = {
-    header: preload.tableHeader
+    header: preload.tableHeader,
+    cells: preload.elements
   };
   render() {
-    return <div className="resp-table">{this.state.header}</div>;
+    const tableHeader = this.state.header.map(title => (
+      <div className="table-header-cell"> {title}</div>
+    ));
+    const tableRow = this.state.cells.map(element => (
+      <div className={`table-row ${element.attributes.state}`}>
+        <div className="table-body-cell">
+          <span>{element.type} </span>
+          <span>{element.id}</span>
+        </div>
+        <div className="table-body-cell"> {element.attributes.owner}</div>
+        <div className="table-body-cell"> {element.attributes.timeStarted}</div>
+        <div className="table-body-cell state"> {element.attributes.state}</div>
+        <div className="table-body-cell">
+          <div className={`square ${element.attributes.metric}`} />
+        </div>
+        <div className="table-body-cell">
+          <div className={`square ${element.attributes.build}`} />
+        </div>
+        <div className="table-body-cell">
+          <div className={`square ${element.attributes.unitTest}`} />
+        </div>
+        <div className="table-body-cell">
+          <div className={`square ${element.attributes.functionalTest}`} />
+        </div>
+      </div>
+    ));
+    return (
+      <div className="resp-table">
+        <div className="table-header">{tableHeader}</div>
+        <div className="table-body">{tableRow}</div>
+      </div>
+    );
   }
 }
 
