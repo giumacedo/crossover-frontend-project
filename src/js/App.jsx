9a9f9cdab1791385e3cda03d4c8e3ad5453b8263
Giulliano Macedo <giulli.macedo@gmail.com>
2017-12-22 16:04:56 -0200

    add basic file structure

---
+++ src/js/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Landing from "./Landing";
+
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route component={FourOhFour} />
+  </Switch>
+);
+
+export default App;
