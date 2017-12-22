84bbc5ac3f356998b2b686e666018f6d4387ec39
Giulliano Macedo <giulli.macedo@gmail.com>
2017-12-22 21:08:50 -0200

    update table component style

--- src/js/Landing.jsx
+++ src/js/Landing.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Table from './Table';
 
-const Landing = () => (
-  <div className="landing">
-    <Table />
-  </div>
-);
+const Landing = () => <Table />;
 
 export default Landing;
--- src/js/Table.jsx
+++ src/js/Table.jsx
@@ -7,36 +7,43 @@ class Table extends Component {
     cells: preload.elements
   };
   render() {
-    const tableHeader = this.state.header.map(title => (
-      <div className="table-header-cell"> {title}</div>
-    ));
+    const tableHeader = this.state.header.map(title => <div>{title}</div>);
     const tableRow = this.state.cells.map(element => (
       <div className={`table-row ${element.attributes.state}`}>
-        <div className="table-body-cell">
-          <span>{element.type} </span>
-          <span>{element.id}</span>
+        <div>
+          {element.type} {'\u00A0'} {element.id}
         </div>
-        <div className="table-body-cell"> {element.attributes.owner}</div>
-        <div className="table-body-cell"> {element.attributes.timeStarted}</div>
-        <div className="table-body-cell state"> {element.attributes.state}</div>
-        <div className="table-body-cell">
+        <div>
+          {element.attributes.owner}
+          {'\u00A0'}
+        </div>
+        <div>
+          {element.attributes.timeStarted}
+          {'\u00A0'}
+        </div>
+        <div className="state"> {element.attributes.state}</div>
+        <div className="status">
           <div className={`square ${element.attributes.metric}`} />
         </div>
-        <div className="table-body-cell">
+        <div className="status">
           <div className={`square ${element.attributes.build}`} />
         </div>
-        <div className="table-body-cell">
+        <div className="status">
           <div className={`square ${element.attributes.unitTest}`} />
         </div>
-        <div className="table-body-cell">
+        <div className="status">
           <div className={`square ${element.attributes.functionalTest}`} />
         </div>
       </div>
     ));
     return (
-      <div className="resp-table">
-        <div className="table-header">{tableHeader}</div>
-        <div className="table-body">{tableRow}</div>
+      <div className="container">
+        <div className="row">
+          <div className="custom-table">
+            <div className="table-header">{tableHeader}</div>
+            <div className="table-content">{tableRow}</div>
+          </div>
+        </div>
       </div>
     );
   }
