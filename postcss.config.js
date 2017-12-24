0b52a10fa59eba9ba78b2c2d62d796b02a5e0deb
Giulliano Macedo <giulli.macedo@gmail.com>
2017-12-24 15:53:03 -0200

    add autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // eslint-disable-next-line no-use-before-define
+  plugins: [require('autoprefixer')]
+};
--- src/js/__tests__/Table.spec.jsx
+++ src/js/__tests__/Table.spec.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+// import { shallow } from 'enzyme';
 import Table from '../components/Table';
+// import preload from '../../data.json';
 
 test('Table renders correctly', () => {
   const component = renderer.create(<Table />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+// test('Table should render correct amount of tableRows', () => {
+//   const component = shallow(<Table shows={preload} />);
+//   expect(component.find('.table-row').length).toEqual(preload.elements.length);
+// });
